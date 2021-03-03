2aeb87a4890b405b3c2ba9da6d94cc7925846468
Luisa María Ortíz Romero <luisamaria.ortiz17@gmail.com>
2021-03-03 15:00:17 -0600

    Correccion a signo de puntuacion

--- Hojadetrabajo1.py
+++ Hojadetrabajo1.py
@@ -1,8 +1,8 @@
 print("Calculadora de Indice de nasa corporal (IMC)")
 #Almacenamiento del pesp
-peso=input("Ingresa tu peso en kilogramos:")
+peso=input("Ingresa tu peso en kilogramos: ")
 #Almacenamiento de la estatura
-estatura=input("Ingresa tu estatura en metros")
+estatura=input("Ingresa tu estatura en metros: ")
 #Calculo del imc
 imc=round((float(peso)/float(estatura)**2),2)
 #Impresion del resultado
